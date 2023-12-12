dc524f4dd58faa37604ef7ce95da5b18aef6a0ff
LightKrite <egorpartenko@icloud.com>
2023-12-12 15:07:20 +0300

    Первичные настройки проекта, добавление лаунчскрина, и цветов в Assets

---
+++ Helpers/Colors + Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Colors + Extensions.swift
+//  ImageFeed
+//
+//  Created by Egor Partenko on 01.12.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+    static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red}
+    static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
+    static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray}
+    static var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray}
+    static var ypBlue: UIColor { UIColor(named: "YP Blue") ?? UIColor.blue}
+    static var ypWhiteBackground: UIColor { UIColor(named: "YP White 50") ?? UIColor.lightGray}
+}
