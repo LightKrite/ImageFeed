7d76c6d5baea825a3ee61c16aa378ff7d0641c29
LightKrite <egorpartenko@icloud.com>
2023-12-21 18:10:31 +0500

    Создание ImageListCell и начало работы с кастомной ячейкой.

---
+++ ImageList/ImageListCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageListCell.swift
+//  ImageFeed
+//
+//  Created by Egor Partenko on 21.12.2023.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+}
---
+++ ImageList/ImagesListViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Egor Partenko on 01.12.2023.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func configCell(for cell: ImagesListCell) { }
+
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    
+    //количество ячеек в секции
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    //возвращает ячейку
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath) // 1
+            
+            guard let imageListCell = cell as? ImagesListCell else { // 2
+                return UITableViewCell()
+            }
+            
+            configCell(for: imageListCell) // 3
+            return imageListCell // 4
+        }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    
+}
