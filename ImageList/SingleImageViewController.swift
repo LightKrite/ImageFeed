af1aff1c2303464b24f787c31a02ff49449d963d
LightKrite <egorpartenko@icloud.com>
2024-01-08 17:06:33 +0500

    Реализован переход с таблицы к SingleImageView

---
+++ ImageList/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Egor Partenko on 07.01.2024.
+//
+
+import Foundation
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet weak var singleImage: UIImageView!
+}
