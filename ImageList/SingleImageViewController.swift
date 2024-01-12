a50ee73422868d5da3852a3caee0666626948527
LightKrite <egorpartenko@icloud.com>
2024-01-12 17:48:16 +0500

    Закончен урок по ScrollView - реализована кнопка share

--- ImageList/SingleImageViewController.swift
+++ ImageList/SingleImageViewController.swift
@@ -14,19 +14,57 @@ final class SingleImageViewController: UIViewController {
         didSet {
             guard isViewLoaded else { return }
             singleImage.image = image
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet private var singleImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         singleImage.image = image
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        rescaleAndCenterImageInScrollView(image: image)
     }
     
+    @IBAction func didTapShareButton(_ sender: UIButton) {
+        shareImage(image: image)
+    }
     
     @IBAction private func didTapBackwardButton() {
         dismiss(animated: true, completion: nil)
     }
     
+    private func rescaleAndCenterImageInScrollView (image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+    
+    private func shareImage(image: UIImage) {
+        let imgToShare = image
+        let items = [imgToShare]
+        let ac = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        ac.popoverPresentationController?.sourceView = self.view
+        present(ac, animated: true, completion: nil)
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        singleImage
+    }
 }
